f6df6cc31f84ca8fb8ca84a0483bc917c2d47d89
MeaDana <111124464+MeaDana@users.noreply.github.com>
2025-10-23 13:42:45 +0300

    Program.cs

--- Program.cs
+++ Program.cs
@@ -11,49 +11,38 @@ namespace EchoServer
         private static readonly ManualResetEvent _shutdownEvent = new ManualResetEvent(false);
 
         static async Task Main(string[] args)
-        {
-            Console.WriteLine("=== Лабораторная работа: TCP и HTTP серверы на C# ===\n");
-            
-            // Настройка обработки Ctrl+C
             Console.CancelKeyPress += (sender, e) =>
             {
                 e.Cancel = true;
                 Console.WriteLine("\nПолучен сигнал завершения...");
                 _shutdownEvent.Set();
             };
-
-            // Запуск TCP сервера
+        
             _tcpServer = new TCPServer();
             var tcpTask = Task.Run(() => _tcpServer.StartAsync());
 
-            // Запуск HTTP сервера
             _httpServer = new SimpleHTTPServer();
             var httpTask = Task.Run(() => _httpServer.StartAsync());
 
-            // Даем серверам время на запуск
             await Task.Delay(1000);
 
             Console.WriteLine("\nСерверы запущены!");
             Console.WriteLine("1. TCP эхо-сервер: 127.0.0.1:8888");
             Console.WriteLine("2. HTTP сервер: http://localhost:8080");
-            Console.WriteLine("3. Для тестирования TCP клиента запустите: dotnet run -- client");
-            Console.WriteLine("4. Для остановки нажмите Ctrl+C\n");
+            Console.WriteLine("3. Для остановки нажмите Ctrl+C\n");
 
-            // Если передан аргумент "client", запускаем клиента
             if (args.Length > 0 && args[0] == "client")
             {
                 await RunTCPClientDemo();
             }
             else
             {
-                // Демонстрация работы TCP клиента
                 _ = Task.Run(RunTCPClientDemo);
                 
                 Console.WriteLine("Ожидание запросов...");
                 _shutdownEvent.WaitOne();
             }
 
-            // Корректное завершение
             Console.WriteLine("Завершение работы серверов...");
             _tcpServer.Stop();
             _httpServer.Stop();
@@ -64,7 +53,7 @@ namespace EchoServer
 
         private static async Task RunTCPClientDemo()
         {
-            await Task.Delay(2000); // Даем серверу время на запуск
+            await Task.Delay(2000);
             
             Console.WriteLine("\n--- Демонстрация TCP клиента ---");
             
